fed0351dfd15eab1bd5c21f3c2ea037c79fc0686
Miguel Angel <miguelrodriguez.vanegasb@gmail.com>
2022-06-12 19:47:29 -0500

    añadir archivos JS

---
+++ FundamentosJS-FIN/js/08.js
@@ -0,0 +1,10 @@
+// Arrays o Arreglos
+const arrow = ['^', 'v',">","<"]
+
+
+//console.log(tecnologias[10])
+console.table(arrow)
+
+
+//console.log(tecnologias.length)
+//console.log(tecnologias.toString())
---
+++ FundamentosJS-FIN/js/17.js
@@ -0,0 +1,35 @@
+// Condicionales
+
+const disponible = 4000
+const retirar = 200
+const a=3
+const b=1
+const c=6
+const d=2
+
+
+
+
+if( b > a )
+ {
+    // Se cumpla la condición y se ejecuta el código
+    console.log(c+d,end=",")
+    console.log(a+b)
+
+} else 
+  {
+    if(a==b)
+    console.log(a,d)
+    else
+    {
+    
+        console.log(a,b,c,d,sep="")
+    }
+  }
+
+/**
+ *  > - Mayor Que
+ *  < - Menor que
+ *  >=  Mayor o igual
+ *  <= Menor o igual
+ */
